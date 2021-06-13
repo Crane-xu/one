00e293d36d19f7e6acd363ee71ee642dfaf260ea
Crane-xu <68410108+Crane-xu@users.noreply.github.com>
2021-06-13 20:24:33 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+// 添加session模块
+var session = require('express-session');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// 改写 从bin目录下www文件改至app.js运行
+var http = require('http');
+var server = http.createServer(app);
+
+// 配置session
+app.use(session({
+  secret: 'keyboard cat',
+  name: 'visitername',
+  resave: false,
+  rolling: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 30 * 60 * 1000 }
+}));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 允许跨域
+app.all('*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  // 带这一行会不解释HTML 返回源码
+  // res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// 监听3000端口
+server.listen('3000');
+
+module.exports = app;
