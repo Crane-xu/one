69dffc6668fc7f11cbda639204f5bdff8ccaa4eb
Crane-xu <68410108+Crane-xu@users.noreply.github.com>
2021-07-29 16:14:22 +0800

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,7 +7,7 @@ function logout(){
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status < 200 || xhr.status >= 300 && xhr.status !== 304) {
-                alert(xhr.status);
+                //alert(xhr.status);
                 console.log('服务器异常');
                 return;
             } else {
